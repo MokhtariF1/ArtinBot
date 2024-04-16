0212d304de50cadf596c135f27b812247d4d326f
MokhtariF1 <157381505+MokhtariF1@users.noreply.github.com>
2024-04-16 18:52:09 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import sqlite3
 con = sqlite3.connect("bot.db")
 cur = con.cursor()
+cur.execute("DROP TABLE pay")
+cur.execute("DROP TABLE users")
 cur.execute("CREATE TABLE users(id,lang,lastd)")
 cur.execute("CREATE TABLE pay(user_id,pay_id,order_id,type)")
