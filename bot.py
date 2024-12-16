decb765c56d610ae542170314754a41c2b8dc709
Ho3einMokhtari <hosseinmokhtari5555@gmail.com>
2024-12-16 17:08:00 +0330

    change amar

--- bot.py
+++ bot.py
@@ -1779,7 +1779,7 @@ async def pay(event):
                 keys = get_public_keys(user_id)
                 await event.reply(bot_text["select"], buttons=keys)
             else:
-                users = cur.execute('SELECT COUNT(*) FROM users').fetchone()[0]
+                users = 111
                 await event.reply(bot_text["statistics_text"].format(users=users))
         # elif text == bot_text["fia_info_management"]:
         #     ch_admin = check_admin(user_id=user_id)
