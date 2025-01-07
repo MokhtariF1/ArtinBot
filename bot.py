a0aba7206a4530e8bac5b5e7d50efc58b380f494
Ho3einMokhtari <hosseinmokhtari5555@gmail.com>
2025-01-07 17:26:07 +0330

    test

--- bot.py
+++ bot.py
@@ -8427,7 +8427,7 @@ async def pay(event):
                 for i in reply_data:
                     # make button with year and gp and event
                     if i["event"] == "Practice_1" or i["event"] == "Practice_2" or i["event"] == "Practice_3" or i["event"] == "Qualifying":
-                        if i["summary"] is None:
+                        if len(i["summary"].keys()) == 0:
                             buttons.append([Button.inline(f"{i['year']} {i['gp']} {i['event']}", str.encode('reply:' + str(i["_id"])))])
                         else:
                             buttons.append([Button.inline(f"{i['year']} {i['gp']} {i['event']} {i['summary']}", str.encode('summary_reply:' + str(i["_id"])))])
